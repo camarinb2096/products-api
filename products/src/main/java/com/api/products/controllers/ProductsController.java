df77a471aa0209a7e9de1c714355b5725ad47263
Alejandro Marin Bustamante <111027925+camarinb2096@users.noreply.github.com>
2024-10-17 02:15:55 +0000

    feat: implement get by id method

--- products/src/main/java/com/api/products/controllers/ProductsController.java
+++ products/src/main/java/com/api/products/controllers/ProductsController.java
@@ -2,9 +2,12 @@ package com.api.products.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
+import java.util.UUID;
+import java.util.Optional;
 
 import com.api.products.services.ProductsServices;
 import com.api.products.entities.ProductsEntity;
@@ -22,6 +25,11 @@ public class ProductsController {
     @GetMapping()
     public List<ProductsEntity> getAllProducts(){
         return productsServices.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<ProductsEntity> getProductById(@PathVariable UUID id) {
+        return productsServices.getProductById(id);
     }
     
 }
